a58c50715fbe5a6f5623e54bf0a08106cb7b0f04
zhdbk3 <zhdbk3@outlook.com>
2024-03-14 22:35:53 +0800

    色阶

--- src/clear.py
+++ src/clear.py
@@ -1,17 +1,42 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('../test/1.jpg')
 
-# 增加对比度和亮度
-alpha = 1.5  # 控制对比度 (1.0 表示不变)
-beta = 0  # 控制亮度 (0 表示不变)
-adjusted = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
 
-# 高反差保留滤波
-filtered = cv2.detailEnhance(adjusted, sigma_s=10, sigma_r=0.15)
+# 色阶
+def adjust_levels(image: np.ndarray, black_level: int, white_level: int) -> np.ndarray:
+    """
+    调整色阶
+    :param image: 图像
+    :param black_level: 黑场
+    :param white_level: 白场
+    :return: 调整后的图像
+    """
+    lut = np.zeros(256, dtype=np.uint8)
+    for i in range(256):
+        if i < black_level:
+            lut[i] = 0
+        elif i > white_level:
+            lut[i] = 255
+        else:
+            lut[i] = (i - black_level) * 255 / (white_level - black_level)
+    image = cv2.LUT(image, lut)
+    return image
+
+
+image = adjust_levels(image, 10, 245)
+
+# 对比度
+alpha = 1.2  # 对比度（1.0表示不变）
+beta = 0  # 亮度（0表示不变）
+image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+# 高反差保留
+image = cv2.detailEnhance(image, sigma_s=10, sigma_r=0.15)
 
 # 写入并显示处理后的图像
-cv2.imwrite('enhanced.jpg', filtered)
-cv2.imshow('Enhanced Image', filtered)
+cv2.imwrite('clear.jpg', image)
+cv2.imshow('Clear Image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
